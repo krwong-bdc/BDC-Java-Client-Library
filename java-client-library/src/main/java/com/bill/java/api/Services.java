a1abcfe97e34c5924ae40198de95f5df30f8888a
Keith R. Wong <krwong@hq.bill.com>
2019-09-11 19:35:34 -0700

    Add AccountsPayable services

--- java-client-library/src/main/java/com/bill/java/api/Services.java
+++ java-client-library/src/main/java/com/bill/java/api/Services.java
@@ -1,10 +1,13 @@
 package com.bill.java.api;
 
 import com.bill.java.api.exception.BDCException;
+import com.bill.java.api.models.AccountsPayableSummary;
 import com.bill.java.api.models.AccountsReceivableSummary;
 import com.bill.java.api.models.ReceivedPay;
+import com.bill.java.api.models.SentPay;
 import com.bill.java.api.net.ApiResource;
 import com.bill.java.api.param.ChargeCustomerRequestParams;
+import com.bill.java.api.param.PayBillsRequestParams;
 
 import java.io.IOException;
 
@@ -16,8 +19,14 @@ public class Services extends ApiResource {
     /** The URI for charging a customer through the BDC API {@value} */
     public static final String CHARGE_CUSTOMER_URL = "/ChargeCustomer.json";
 
-    /** The URI for retrieving a summary of the user's accounts receivable invoices and received payments */
+    /** The URI for retrieving a summary of the user's accounts receivable invoices and received payments {@value}*/
     public static final String GET_AR_SUMMARY_URL = "/GetARSummary.json";
+
+    /** The URI for paying a vendor through the BDC API {@value} */
+    public static final String PAY_BILLS_URL = "PayBills.json";
+
+    /** The URI for retrieving a summary of the user's accounts receivable invoices and received payments {@value}*/
+    public static final String GET_AP_SUMMARY_URL = "/GetAPSummary.json";
 
     /**
      * This processes a receipt from a customer to withdraw funds from the authorized CustomerBankAccount.
@@ -43,7 +52,35 @@ public class Services extends ApiResource {
      * @throws BDCException when the response from the API is unsuccessful
      * @throws IOException  when an I/O exception occurs on the underlying request
      */
-    public static AccountsReceivableSummary getARSummary() throws IOException, BDCException {
+    public static AccountsReceivableSummary getAccountsReceivableSummary() throws IOException, BDCException {
         return create(GET_AR_SUMMARY_URL, AccountsReceivableSummary.class);
+    }
+
+    /**
+     * Use this API to issue Bill.com payments (one or more bills and one or more bill credits) to a designated vendor. These are paid from a specified bank account.
+     * <p>
+     * The user that uses this API MUST be verified ("pay bills" function enabled in the assigned user role - Administrator, Payer, customized). If not, an error is returned.
+     *
+     * @param payBillsRequestParams data required to make the request
+     * @return                      a SentPay record
+     * @throws BDCException         when the response from the API is unsuccessful
+     * @throws IOException          when an I/O exception occurs on the underlying request
+     */
+    public static SentPay chargeCustomer(PayBillsRequestParams payBillsRequestParams) throws IOException, BDCException {
+        if(payBillsRequestParams == null) {
+            throw new NullPointerException("PayBillsRequestParams required.");
+        }
+        return create(PAY_BILLS_URL, payBillsRequestParams, SentPay.class);
+    }
+
+    /**
+     * This returns a summary of all Accounts Payable bills and approvals for your Bill.com account.
+     *
+     * @return              an AccountsPayableSummary object
+     * @throws BDCException when the response from the API is unsuccessful
+     * @throws IOException  when an I/O exception occurs on the underlying request
+     */
+    public static AccountsPayableSummary getAccountsPayableSummary() throws IOException, BDCException {
+        return create(GET_AR_SUMMARY_URL, AccountsPayableSummary.class);
     }
 }
